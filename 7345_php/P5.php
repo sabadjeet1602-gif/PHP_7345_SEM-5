c3ddcf17b1513cb13405674cfd799abc06511a79
sabadjeet1602-gif <sabadjeet1602@gmail.com>
2026-06-09 12:56:41 +0530

    Add files via upload

---
+++ 7345_php/P5.php
@@ -0,0 +1,21 @@
+#For <br>
+
+<?php
+
+for ($i = 5; $i <= 10; $i++) {
+    echo $i . "<br>";
+}
+
+?>
+
+#Foreach <br>
+
+<?php
+
+$numbers = array(5, 6, 7, 8, 9, 10);
+
+foreach ($numbers as $num) {
+    echo $num . "<br>";
+}
+
+?>
