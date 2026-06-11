a25c064c32ddb438d02bea1cb692aa9307c4f52b
sabadjeet1602-gif <sabadjeet1602@gmail.com>
2026-06-11 12:44:06 +0530

    Add files via upload

---
+++ 7345_php/MINI & MAX NUM.php
@@ -0,0 +1,14 @@
+<?php
+
+$num1 = 5;
+$num2 = 10;
+
+if ($num1 > $num2) {
+    echo "Maximum Number = " . $num1 . "<br>";
+    echo "Minimum Number = " . $num2;
+} else {
+    echo "Maximum Number = " . $num2 . "<br>";
+    echo "Minimum Number = " . $num1;
+}
+
+?>
---
+++ 7345_php/STUDENT INFO.php
@@ -0,0 +1,21 @@
+<?php
+
+define("COLLEGE_NAME", "MARWADI UNIVERSITY");
+
+$studentName = "Sabad Jeet";
+$rollNo = 101;
+$semester = "Semester 4";
+$marks = 490;
+$totalMarks = 500;
+$percentage = ($marks / $totalMarks) * 100;
+
+echo "<h2>Previous Semester Result</h2>";
+echo "College Name: " . COLLEGE_NAME . "<br>";
+echo "Student Name: " . $studentName . "<br>";
+echo "Roll Number: " . $rollNo . "<br>";
+echo "Semester: " . $semester . "<br>";
+echo "Marks Obtained: " . $marks . "<br>";
+echo "Total Marks: " . $totalMarks . "<br>";
+echo "Percentage: " . $percentage . "%<br>";
+
+?>
